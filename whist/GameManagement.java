f9699fef03849f86dc36a9bddd8bbe80eeec642b
Jiexin Bao <271039949@qq.com>
2020-06-04 23:50:24 +0800

    1

--- whist/GameManagement.java
+++ whist/GameManagement.java
@@ -168,12 +168,12 @@ public class GameManagement extends CardGame implements IGameManagement,PlayersM
         else if(GameInformation.players.get(nextPlayer) == "NPC"){
     		setStatusText("Player " + nextPlayer + " thinking...");
             delay(GameInformation.thinkingTime);
-			selected = PlayersMode.NPCSelection(GameInformation.hands[nextPlayer],lead,trumps);
+			selected = NPCSelection(GameInformation.hands[nextPlayer],lead,trumps);
 			System.out.println("NPC Card" + selected);
         }
         else if(GameInformation.players.get(nextPlayer) == "Smart_NPC"){
 			Card largestTrump = largestTrumpOnBoard(GameInformation.currentCards,trumps);
-			selected = PlayersMode.SmartSelection(GameInformation.hands[nextPlayer],lead,trumps,hasTrumpOnBoard,winningCard,largestTrump);
+			selected = SmartSelection(GameInformation.hands[nextPlayer],lead,trumps,hasTrumpOnBoard,winningCard,largestTrump);
 			System.out.println("Smart Card " + selected);
 		}
         //New
@@ -204,12 +204,12 @@ public class GameManagement extends CardGame implements IGameManagement,PlayersM
 		        delay(GameInformation.thinkingTime);
 		        //selected = randomCard(hands[nextPlayer]);
 
-				selected = PlayersMode.NPCSelection(GameInformation.hands[nextPlayer],lead,trumps);
+				selected = NPCSelection(GameInformation.hands[nextPlayer],lead,trumps);
 				System.out.println("NPC Card" + selected);
 	        }
 	        else if(GameInformation.players.get(nextPlayer) == "Smart_NPC"){
 	        	Card largestTrump = largestTrumpOnBoard(GameInformation.currentCards,trumps);
-				selected = PlayersMode.SmartSelection(GameInformation.hands[nextPlayer],lead,trumps,hasTrumpOnBoard,winningCard,largestTrump);
+				selected = SmartSelection(GameInformation.hands[nextPlayer],lead,trumps,hasTrumpOnBoard,winningCard,largestTrump);
 				System.out.println("Smart Card " + selected);
 			}
 	        //New
@@ -297,6 +297,106 @@ public class GameManagement extends CardGame implements IGameManagement,PlayersM
 	}
   	return largestTrumpCard;
   }
+
+	/** New Function used for normal NPC to follow the rules*/
+	@Override
+	public Card NPCSelection(Hand hand, CardsInformation.Suit lead, CardsInformation.Suit trumps){
+		if (lead != null) {
+			int cardNumber = hand.getNumberOfCards();
+			for (int i = 0; i < cardNumber; i++) {
+				if (hand.get(i).getSuit() == lead) {
+					return hand.get(i);
+				} else continue;
+			}
+		}
+		return GameInformation.randomCard(hand);
+
+	}
+
+	/** New Function used for Smart AI to select cards.
+	 * When taking the lead, it will select the card of highest number in hand, or the trump card of highest number
+	 * in hand if any.
+	 * When there is no leading card, it will try to select the largest trump card in hand.
+	 * When there is no card can win the game, it will select the smallest card.*/
+	@Override
+	public Card SmartSelection(Hand hand, CardsInformation.Suit lead,
+							   CardsInformation.Suit trumps, boolean hasTrumpOnBoard,
+							   Card winningCard, Card largestTrump){
+		Card trumpCard = null;
+		Card maxNormalCard = null;
+		Card minNormalCard = null;
+		Card smallestCard = null;
+		//When it is the first to play
+		if(lead == null){
+			for(int i = 0; i < hand.getNumberOfCards(); i++){
+				if(hand.get(i).getSuit() == trumps){
+					if(trumpCard == null || GameInformation.rankGreater(hand.get(i),trumpCard)){
+						trumpCard = hand.get(i);
+					}
+				}
+				if(maxNormalCard == null || GameInformation.rankGreater(hand.get(i), maxNormalCard)){
+					maxNormalCard = hand.get(i);
+				}
+			}
+
+			if(trumpCard != null){
+				return trumpCard;
+			}
+			else return maxNormalCard;
+		}
+		//When it is not the first one to play
+		else {
+			for(int i = 0; i < hand.getNumberOfCards(); i++){
+				if(hand.get(i).getSuit() == trumps){
+					if(trumpCard == null || GameInformation.rankGreater(hand.get(i),trumpCard)){
+						trumpCard = hand.get(i);
+					}
+				}
+				if(hand.get(i).getSuit() == lead){
+					if(maxNormalCard == null || GameInformation.rankGreater(hand.get(i),maxNormalCard)){
+						maxNormalCard = hand.get(i);
+					}
+					if(minNormalCard == null || GameInformation.rankGreater(minNormalCard, hand.get(i))){
+						minNormalCard = hand.get(i);
+					}
+				}
+				if(smallestCard == null || GameInformation.rankGreater(smallestCard,hand.get(i))){
+					smallestCard = hand.get(i);
+				}
+			}
+			//If it has leading suit cards in hand.
+			if(maxNormalCard != null){
+				if(hasTrumpOnBoard){
+					return minNormalCard;
+				}
+				if(GameInformation.rankGreater(maxNormalCard,winningCard)){
+					return maxNormalCard;
+				}
+				else return minNormalCard;
+			}
+			//If there is no leading suit card in hand
+			else {
+				if(trumpCard != null){
+					if(!hasTrumpOnBoard){
+						return trumpCard;
+					}
+					else {
+						Card curTrumpOnBoard = largestTrump;
+						if (GameInformation.rankGreater(trumpCard,curTrumpOnBoard)){
+							return trumpCard;
+						}
+						else {
+							return smallestCard;
+						}
+					}
+				}
+				else {
+					return smallestCard;
+				}
+			}
+		}
+	}
+
 
 
 }
--- whist/PlayersMode.java
+++ whist/PlayersMode.java
@@ -7,97 +7,15 @@ import ch.aplu.jcardgame.Hand;
  * Can be extended in the future.
  */
 public interface PlayersMode {
-    /** New Function used for normal NPC to follow the rules*/
-    public static Card NPCSelection(Hand hand, CardsInformation.Suit lead, CardsInformation.Suit trumps){
-        if (lead != null) {
-            int cardNumber = hand.getNumberOfCards();
-            for (int i = 0; i < cardNumber; i++) {
-                if (hand.get(i).getSuit() == lead) {
-                    return hand.get(i);
-                } else continue;
-            }
-        }
-        return GameInformation.randomCard(hand);
+    /**
+     * New Function used for normal NPC to follow the rules
+     */
+    abstract Card NPCSelection(Hand hand, CardsInformation.Suit lead, CardsInformation.Suit trumps);
 
-    }
-    /** New Function used for Smart AI to select cards.
-     * When taking the lead, it will select the card of highest number in hand, or the trump card of highest number
-     * in hand if any.
-     * When there is no leading card, it will try to select the largest trump card in hand.
-     * When there is no card can win the game, it will select the smallest card.*/
-    public static Card SmartSelection(Hand hand, CardsInformation.Suit lead, CardsInformation.Suit trumps, boolean hasTrumpOnBoard, Card winningCard, Card largestTrump){
-        Card trumpCard = null;
-        Card maxNormalCard = null;
-        Card minNormalCard = null;
-        Card smallestCard = null;
-        //When it is the first to play
-        if(lead == null){
-            for(int i = 0; i < hand.getNumberOfCards(); i++){
-                if(hand.get(i).getSuit() == trumps){
-                    if(trumpCard == null || GameInformation.rankGreater(hand.get(i),trumpCard)){
-                        trumpCard = hand.get(i);
-                    }
-                }
-                if(maxNormalCard == null || GameInformation.rankGreater(hand.get(i), maxNormalCard)){
-                    maxNormalCard = hand.get(i);
-                }
-            }
-
-            if(trumpCard != null){
-                return trumpCard;
-            }
-            else return maxNormalCard;
-        }
-        //When it is not the first one to play
-        else {
-            for(int i = 0; i < hand.getNumberOfCards(); i++){
-                if(hand.get(i).getSuit() == trumps){
-                    if(trumpCard == null || GameInformation.rankGreater(hand.get(i),trumpCard)){
-                        trumpCard = hand.get(i);
-                    }
-                }
-                if(hand.get(i).getSuit() == lead){
-                    if(maxNormalCard == null || GameInformation.rankGreater(hand.get(i),maxNormalCard)){
-                        maxNormalCard = hand.get(i);
-                    }
-                    if(minNormalCard == null || GameInformation.rankGreater(minNormalCard, hand.get(i))){
-                        minNormalCard = hand.get(i);
-                    }
-                }
-                if(smallestCard == null || GameInformation.rankGreater(smallestCard,hand.get(i))){
-                    smallestCard = hand.get(i);
-                }
-            }
-            //If it has leading suit cards in hand.
-            if(maxNormalCard != null){
-                if(hasTrumpOnBoard){
-                    return minNormalCard;
-                }
-                if(GameInformation.rankGreater(maxNormalCard,winningCard)){
-                    return maxNormalCard;
-                }
-                else return minNormalCard;
-            }
-            //If there is no leading suit card in hand
-            else {
-                if(trumpCard != null){
-                    if(!hasTrumpOnBoard){
-                        return trumpCard;
-                    }
-                    else {
-                        Card curTrumpOnBoard = largestTrump;
-                        if (GameInformation.rankGreater(trumpCard,curTrumpOnBoard)){
-                            return trumpCard;
-                        }
-                        else {
-                            return smallestCard;
-                        }
-                    }
-                }
-                else {
-                    return smallestCard;
-                }
-            }
-        }
-    }
+    /**
+     * New Function used for Smart AI to select cards.
+     */
+    abstract Card SmartSelection(Hand hand, CardsInformation.Suit lead,
+                                 CardsInformation.Suit trumps, boolean hasTrumpOnBoard,
+                                 Card winningCard, Card largestTrump);
 }
